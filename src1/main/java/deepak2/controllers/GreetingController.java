469f2f18b9fe4d54527304c791ce4768c5107f5b
deepak <deepakpappur@gmail.com>
2019-08-06 19:07:27 -0700

    deepak project2

---
+++ src1/main/java/deepak2/controllers/GreetingController.java
@@ -0,0 +1,20 @@
+package deepak2.controllers;
+
+import deepak2.models.Greeting;
+import leap.core.validation.annotations.Required;
+import leap.web.annotation.DefaultValue;
+import leap.web.annotation.http.GET;
+import leap.web.api.mvc.ApiController;
+import leap.web.api.mvc.ApiResponse;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class GreetingController extends ApiController {
+
+    private static final AtomicInteger counter = new AtomicInteger();
+
+    @GET
+    public ApiResponse<Greeting> greeting(@Required @DefaultValue("World") String name) {
+        return ApiResponse.of(new Greeting(counter.incrementAndGet(), String.format("Hello, %s!", name)));
+    }
+
+}
